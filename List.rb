928377f82f5e4fc759acd3992eeb4a9bed9fbfc7
AVGN Sevich <AVGN Sevich>
2016-09-19 21:35:18 +0300

    Shoes app: 1.2

--- List.rb
+++ List.rb
@@ -3,6 +3,12 @@ Shoes.app(title: "TODO List",
 
 	background "#feff54".."#ff0067"
 	@welcome = para "TODO List", size: 35, align: "center", underline: "single"
+	stack do
+		@new_item = button "New Item"
+		@new_item.click() do 
+			@item = edit_line width: 400
+		end
+	end
 
 }
 
