384cb427b9e8585ebad9dd61dcc74a59c697bedc
AVGN Sevich <AVGN Sevich>
2016-09-19 21:13:59 +0300

    Update 1.1

--- List.rb
+++ List.rb
@@ -1,3 +1,13 @@
-Shoes.app(width: 500, height: 500) {
-	@welcome = para "TODO List by Seva"
+
+Shoes.app(title: "TODO List",
+   	      width: 400, height: 500) {
+
+	background "#feff54".."#ff0067"
+	@welcome = para "TODO List", size: 35, align: "center", underline: "single"
+
 }
+
+
+
+
+
