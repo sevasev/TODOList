b6091e625fcc7c136f867d3f1bb0ade3441b0470
Sewa <richello3027@gmail.com>
2016-10-31 19:39:00 +0400

    1.3.1
    It seems to be more friendly!

--- ToDoList.rb
+++ ToDoList.rb
@@ -2,26 +2,40 @@
 
 Shoes.app :width => 500, :height => 600 do
 
-	background "#FFF"
+	flow :width => 1.0, :height => 1.0 do
 
-	stack :height => 0.3, :width => 1.0 do
+		background "#fdfdfd"
 
-		banner "Things to do:", :align => "center", :underline => "single", :stroke => "#c4410d"
-		
-		@item_text = edit_line :width => 1.0, :height => 50, :margin_left => 0.2, :margin_right => 0.2, :margin_bottom => 10
-		
-		@append_item = button "New Item...", :width => 1.0, :height => 40, :margin_left => 0.2, :margin_right => 0.2
-		
-		@append_item.click do 
-			@list.append do
-				para @item_text.text
+		stack :height => 0.3, :width => 1.0 do
+
+			@banner = banner "Things to do:", :align => "center", 
+			:stroke => "#c4410d", :margin_top => 10
+
+			@item_text = edit_line :width => 1.0, :height => 45, 
+			:margin_left => 0.1, :margin_right => 0.1
+
+			@add_item = button("Add Item", :width => 1.0, :height => 40,
+			:margin_left => 0.1, :margin_right => 0.1)
+
+			@items_amount = 0
+
+			@add_item.click do
+
+				@items_amount += 1
+
+				@index.append do
+					para @items_amount, '. ', @item_text.text
+				end
+
+				@item_text.text = ''
+
 			end
-			@item_text.text = ''
-		end
-	
-	end
 
-	@list = stack :height => 0.7, :width => 1.0 do;	end
+		end
+
+		@index = stack :height => 0.7, :width =>  1.0 do; end
+
+	end
 
 end
 
